4cfbb6dcb7eb2e3c07b38464259d8603f41f88d9
tommasobruno <bruno.tommaso@protonmail.com>
2024-05-29 17:32:44 +0100

    feat: add flake.nix for environment

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "z_engine";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default =
+        pkgs.mkShell { nativeBuildInputs = with pkgs; [ zig zls glfw ]; };
+    };
+}
