a6c44781daa8a392e2281450a4a2bd677d5f7f36
tommasobruno <bruno.tommaso@protonmail.com>
2024-06-01 23:06:58 +0100

    chore: move window to own file

---
+++ src/core/window.zig
@@ -0,0 +1,53 @@
+const std = @import("std");
+const c = @import("../c.zig");
+
+const WindowBufferError = error{ WindowInizialization, GLFWInitialization, GLADInitialization, OpenGLVersion };
+
+pub const WindowBuffer = struct {
+    width: c_int,
+    height: c_int,
+
+    window: *c.GLFWwindow,
+
+    fn loadGL() WindowBufferError!void {
+        if (c.gladLoadGL() == 0) {
+            return WindowBufferError.GLADInitialization;
+        }
+
+        const version_ptr = c.glGetString(c.GL_VERSION);
+
+        if (version_ptr == null) {
+            return WindowBufferError.OpenGLVersion;
+        }
+
+        std.debug.print("Using OpenGL version {s}\n", .{version_ptr});
+    }
+
+    pub fn init(width: c_int, height: c_int) WindowBufferError!WindowBuffer {
+        if (c.glfwInit() == c.GLFW_FALSE) {
+            return WindowBufferError.GLFWInitialization;
+        }
+
+        // Set OpenGL version
+        c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 4);
+        c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 1);
+        c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
+
+        const window = c.glfwCreateWindow(width, height, "Z_Engine", null, null) orelse {
+            c.glfwTerminate();
+            return WindowBufferError.WindowInizialization;
+        };
+
+        c.glfwMakeContextCurrent(window);
+        c.glfwSwapInterval(0);
+
+        try loadGL();
+
+        return .{ .width = width, .height = height, .window = window };
+    }
+
+    pub fn deinit(self: WindowBuffer) void {
+        c.glfwTerminate();
+        c.glfwDestroyWindow(self.window);
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,42 +1,10 @@
 const std = @import("std");
 const c = @import("c.zig");
+const windowBuffer = @import("core/window.zig").WindowBuffer;
 
 pub fn main() !void {
-    if (c.glfwInit() == c.GLFW_FALSE) {
-        std.log.err("Failed to initialize GLFW\n", .{});
-        return;
-    }
-
-    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 4);
-    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 1);
-    c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
-
-    const window = c.glfwCreateWindow(640, 480, "Z_Engine", null, null) orelse {
-        c.glfwTerminate();
-        return;
-    };
-
-    defer {
-        c.glfwDestroyWindow(window);
-        c.glfwTerminate();
-    }
-
-    c.glfwMakeContextCurrent(window);
-
-    if (c.gladLoadGL() == c.GLFW_FALSE) {
-        std.log.err("Failed to initialize glad\n", .{});
-        return;
-    }
-
-    const version_ptr = c.glGetString(c.GL_VERSION);
-
-    if (version_ptr == null) {
-        std.log.err("Could not get openGL version", .{});
-    }
-
-    std.debug.print("{s}\n", .{version_ptr});
-
-    c.glfwSwapInterval(1);
+    const window = try windowBuffer.init(600, 460);
+    defer window.deinit();
 
     // Shaders
     const vertex_shader_source: [*c]const u8 =
@@ -47,6 +15,7 @@ pub fn main() !void {
         \\  gl_Position = gl_Position = vec4(aPos, 1.0);
         \\}
     ;
+
     const vertex_shader = c.glCreateShader(c.GL_VERTEX_SHADER);
     c.glShaderSource(vertex_shader, 1, &vertex_shader_source, null);
     c.glCompileShader(vertex_shader);
@@ -66,7 +35,7 @@ pub fn main() !void {
         \\out vec4 fragment;
         \\void main()
         \\{
-        \\  fragment = vec4(1.0, 0.0, 0.0, 1.0);
+        \\  fragment = vec4(1.0, 1.0, 1.0, 1.0);
         \\}
     ;
     const fragment_shader = c.glCreateShader(c.GL_FRAGMENT_SHADER);
@@ -76,7 +45,7 @@ pub fn main() !void {
     c.glGetShaderiv(fragment_shader, c.GL_COMPILE_STATUS, &success);
 
     if (success == 0) {
-        c.glGetShaderInfoLog(vertex_shader, 512, null, &log[0]);
+        c.glGetShaderInfoLog(fragment_shader, 512, null, &log[0]);
         std.log.err("Error compiling fragment: {s}\n", .{&log});
     }
 
@@ -90,7 +59,7 @@ pub fn main() !void {
     c.glDeleteShader(vertex_shader);
     c.glDeleteShader(fragment_shader);
 
-    const vertices: [9]f32 = [_]f32{ -0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0 };
+    const vertices = [_]f32{ -0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0 };
 
     var VBO: c.GLuint = 0;
     var VAO: c.GLuint = 0;
@@ -112,22 +81,16 @@ pub fn main() !void {
     c.glBindVertexArray(0);
     c.glDisableVertexAttribArray(0);
 
-    c.glClearColor(0.0, 0.0, 1.0, 1.0);
-    while (c.glfwWindowShouldClose(window) == c.GLFW_FALSE) {
-        var height: c_int = undefined;
-        var width: c_int = undefined;
+    c.glClearColor(0.0, 0.10, 0.07, 1.0);
 
-        c.glfwGetFramebufferSize(window, &width, &height);
-        c.glViewport(0, 0, width, height);
-
+    while (c.glfwWindowShouldClose(window.window) == c.GLFW_FALSE) {
         c.glClear(c.GL_COLOR_BUFFER_BIT);
-
         // Draw stuff
         c.glUseProgram(program);
         c.glBindVertexArray(VAO);
         c.glDrawArrays(c.GL_TRIANGLES, 0, 3);
 
-        c.glfwSwapBuffers(window);
+        c.glfwSwapBuffers(window.window);
         c.glfwPollEvents();
     }
 
